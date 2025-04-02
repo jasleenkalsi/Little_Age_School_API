c7ee39ba49ff40bae8adefe571c586c23c239198
jasleen <jasleenkaurkalsi03@gmail.com>
2025-04-02 11:47:27 -0500

    feat: add firebase admin configuration for authentication in config/firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -2,7 +2,7 @@
 import admin from 'firebase-admin';
 import { credential } from 'firebase-admin';
 
-import * as serviceAccount from '../../little-age-school-api-firebase-adminsdk-fbsvc-4e300966db';
+import * as serviceAccount from '../little-age-school-api-firebase-adminsdk-fbsvc-4e300966db.json';
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
